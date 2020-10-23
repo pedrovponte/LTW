70a3e3fa5b57ffbfe04accebdf085f9c2c6517f9
Pedro <up201809694@fe.up.pt>
2020-10-23 16:04:48 +0100

    Continue TP3

--- Praticas/PHP/Exercicio5/Database/list_news.php
+++ Praticas/PHP/Exercicio5/Database/list_news.php
@@ -15,11 +15,10 @@
     function get_news_complete_information() {
         global $db;
         
-        $stmt = $db->prepare('
-        SELECT news.*, users.*, COUNT(comments.id) AS comments
+        $stmt = $db->prepare('SELECT news.*, users.*, COUNT(comments.id) AS comments
         FROM news JOIN
-        users USING (username) LEFT JOIN
-        comments ON comments.news_id = news.id
+             users USING (username) LEFT JOIN
+             comments ON comments.news_id = news.id
         GROUP BY news.id, users.username
         ORDER BY published DESC');
 
@@ -27,6 +26,15 @@
         $articles = $stmt->fetchAll();
 
         return $articles;
+    }
+
+    function get_article() {
+        global $db;
+
+        $stmt = $db->prepare('SELECT * FROM news JOIN users USING (username) WHERE id = :id');
+        $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+        $stmt->execute();
+        $article = $stmt->fetch();
     }
 
 ?>
---
+++ Praticas/PHP/Exercicio5/Templates/draw_news.php
@@ -0,0 +1,23 @@
+<?php function draw_news($articles) {
+    foreach ($articles as $article) { 
+        $tags = explode(',',$article['tags']); ?>
+        <article>
+            <header>
+                <h1><a href="item.html"> <?= $article['title'] ?></a></h1>
+            </header>
+            <img src="https://dummyimage.com/600x300/008ebd/fff.jpg&text=business" alt="">
+            <p> <?= $article['introduction'] ?> <p>
+            <p> <?= $article['fulltext'] ?> </p>
+            <footer>
+                <span class="author"><?= $article['name'] ?> </span>
+                <span class="tags">
+                <?php foreach($tags as $tag) { ?>
+                    <a href="index.php"><?= $tag ?></a>
+                <?php } ?>
+                </span>
+                <span class="date"><?= date("d-m-Y H:i:s", substr($article['published'],0,10)) ?></span>
+                <a class="comments" href="item.html#comments"><?= $article['comments'] ?></a>
+            </footer>
+      </article>
+    <?php } 
+} ?>
